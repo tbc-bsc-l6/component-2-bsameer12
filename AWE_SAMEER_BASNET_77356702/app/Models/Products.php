d89c71e9164d550077294a226d4e40747c0ab25e
bsameer12 <104411812+bsameer12@users.noreply.github.com>
2025-01-07 07:39:07 +0545

    resources and migration file created.

--- AWE_SAMEER_BASNET_77356702/app/Models/Products.php
+++ AWE_SAMEER_BASNET_77356702/app/Models/Products.php
@@ -2,9 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Products extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'brief_description',
+        'description',
+        'price',
+        'old_price',
+        'SKU',
+        'stock_status',
+        'quantity',
+        'image',
+        'images',
+    ];
+
+    public function categories()
+    {
+        return $this->belongsToMany(Categories::class);
+    }
 }
--- AWE_SAMEER_BASNET_77356702/database/migrations/2024_12_12_041500_create_products_table.php
+++ AWE_SAMEER_BASNET_77356702/database/migrations/2024_12_12_041500_create_products_table.php
@@ -12,15 +12,18 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id(); // Auto-incrementing product ID
-            $table->string('product_name'); // Product name
-            $table->decimal('product_price', 12, 2); // Product price (up to 2 decimal places)
-            $table->unsignedBigInteger('added_by'); // ID of the user who added the product
-            $table->timestamp('added_on')->useCurrent(); // Default current timestamp // Date and time when the product was added
-            $table->integer('quantity'); // Quantity of the product
-            $table->decimal('vat_percent', 5, 2); // VAT percentage (up to 2 decimal places)
-            $table->decimal('total_amount_with_vat', 12, 2); // Total amount with VAT included
-            $table->timestamps(); // Created_at and updated_at timestamps
+            $table->id();
+            $table->string('name');
+            $table->string('brief_description');
+            $table->text('description');
+            $table->decimal('price');
+            $table->decimal('old_price');
+            $table->string('SKU');
+            $table->enum('stock_status', ['instock', 'outstock']);
+            $table->unsignedInteger('quantity')->default(10);
+            $table->string('image');
+            $table->text('images');
+            $table->timestamps();
         });
     }
 
